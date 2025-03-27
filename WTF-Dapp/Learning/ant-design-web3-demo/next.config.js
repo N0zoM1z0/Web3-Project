281b6f8f11912f84debc984d215b219d02578c87
N0zoM1z0 <1705004398@qq.com>
2025-03-27 10:41:15 +0800

    remove and update~

--- WTF-Dapp/Learning/ant-design-web3-demo/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: [  "@ant-design", "antd", "rc-util", "rc-pagination", "rc-picker", "rc-input" ],
-
-}
-
-module.exports = nextConfig
